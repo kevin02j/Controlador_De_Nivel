0e7853522b87fcd326d6babea8c6a576aa1fe3a9
Kevin Campo <kevinerneycertuche@gmail.com>
2024-03-27 21:55:02 -0500

    Se aplico un filtro SMA a las lecturas del sensor, se modifco la manera es como se graficca en AppDesigner. NOTA: corregir el filtro presenta muchos picos indeseados aun, seguir acondicionandolo, para obtener un mejor resultado

--- CodeController/CodeController.ino
+++ CodeController/CodeController.ino
@@ -1,6 +1,12 @@
 #define TRIGGER_PIN 4
 #define ECHO_PIN 2
 
+#define NUM_READINGS 5
+
+int readingsArray[NUM_READINGS];
+int sumReadings = 0;
+int filteredDistance;
+
 void setup() {
   Serial.begin(9600);
   pinMode(TRIGGER_PIN, OUTPUT);
@@ -8,20 +14,33 @@ void setup() {
 }
 
 void loop() {
-  // Envía un pulso corto al pin de trigger para activar el sensor
+  // Enviar pulso y obtener duración
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
-
   long duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Calcular distancia sin filtrar
   int distance = (duration * 0.034) / 2;
 
-  // Imprime la distancia medida
-  Serial.println(distance);
+  // Almacenar nueva lectura en el arreglo
+  for (int i = NUM_READINGS - 1; i > 0; i--) {
+    readingsArray[i] = readingsArray[i - 1];
+  }
+  readingsArray[0] = distance;
 
-  // Espera un momento antes de tomar otra lectura
-  delay(1000);
+  // Calcular suma y promedio de las lecturas
+  sumReadings = 0;
+  for (int i = 0; i < NUM_READINGS; i++) {
+    sumReadings += readingsArray[i];
+  }
+  filteredDistance = sumReadings / NUM_READINGS;
+
+  // Imprimir distancia filtrada
+  Serial.println(filteredDistance);
+
+  delay(100);
 }
 
