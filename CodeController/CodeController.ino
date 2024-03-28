c9a2860778dd0dff52b52bba3f14cd66284a29c9
Kevin Campo <kevinerneycertuche@gmail.com>
2024-03-27 22:16:21 -0500

    Se aplico el filtro Kalman a las medidas del sensor

--- CodeController/CodeController.ino
+++ CodeController/CodeController.ino
@@ -1,46 +1,52 @@
-#define TRIGGER_PIN 4
-#define ECHO_PIN 2
+// #define TRIGGER_PIN 4
+// #define ECHO_PIN 2
+// Pins
+const int echo = 2;
+const int trig = 4;
+// Constrains
+const int maxdist = 335;
+const float mindist = 2.5;
 
-#define NUM_READINGS 5
+double distance, duration;
+double kaldist;
 
-int readingsArray[NUM_READINGS];
-int sumReadings = 0;
-int filteredDistance;
+double kalman(double U) {
+  static const double R = 40;
+  static const double H = 1.00;
+  static double Q = 100;
+  static double P = 0;
+  static double U_hat = 0;
+  static double K = 0;
+  K = P * H / (H * P * H + R);
+  U_hat += +K * (U - H * U_hat);
+  P = (1 - K * H) * P + Q;
+  return U_hat;
+}
 
 void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
   Serial.begin(9600);
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+}
+
+void usonic_transmit() {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
 }
 
 void loop() {
-  // Enviar pulso y obtener duración
-  digitalWrite(TRIGGER_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  long duration = pulseIn(ECHO_PIN, HIGH);
+  usonic_transmit();
 
-  // Calcular distancia sin filtrar
-  int distance = (duration * 0.034) / 2;
+  duration = pulseIn(echo, HIGH);
+  distance = (duration * .034) / 2;
+  kaldist = kalman(distance);
 
-  // Almacenar nueva lectura en el arreglo
-  for (int i = NUM_READINGS - 1; i > 0; i--) {
-    readingsArray[i] = readingsArray[i - 1];
-  }
-  readingsArray[0] = distance;
+  Serial.print("Distance (in cm): ");
+  Serial.println(distance);
+  Serial.print("Corrected distance (in cm): ");
+  Serial.println(kaldist);
 
-  // Calcular suma y promedio de las lecturas
-  sumReadings = 0;
-  for (int i = 0; i < NUM_READINGS; i++) {
-    sumReadings += readingsArray[i];
-  }
-  filteredDistance = sumReadings / NUM_READINGS;
-
-  // Imprimir distancia filtrada
-  Serial.println(filteredDistance);
-
-  delay(100);
+  delay(500);
 }
-
